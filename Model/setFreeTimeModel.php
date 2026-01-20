5d215d8ab5c4f342614bcfd1556ef323e18db6d3
Md Rafit Rahad <rafitrahad@gmail.com>
2026-01-21 05:06:21 +0600

    free slot added

--- Model/setFreeTimeModel.php
+++ Model/setFreeTimeModel.php
@@ -2,15 +2,21 @@
     require_once("dbConnect.php");
 
     function insertDataFreeTime($day,$freeTime,$s_id,$t_id){
-        
-        $query="INSERT INTO free_time ( day,free_times,s_id,t_id) VALUES ('$day','$freeTime','$s_id','$t_id')";
-        $conn=dbConnect();
+        if($t_id==null){
+            $query="INSERT INTO free_time ( day,free_times,s_id,t_id) VALUES ('$day','$freeTime','$s_id',NULL)";
+            $conn=dbConnect();
 
-        $data=mysqli_query($conn,$query);
+            $data=mysqli_query($conn,$query);
+        }else{
+            $query="INSERT INTO free_time ( day,free_times,s_id,t_id) VALUES ('$day','$freeTime',NULL,'$t_id')";
+            $conn=dbConnect();
+
+            $data=mysqli_query($conn,$query);
+        }
+        
     }
     
     function updateFreeTime($day,$freeTime,$s_id,$t_id){
-        
         $query="UPDATE free_time SET free_times='$freeTime' WHERE day='$day' AND s_id='$s_id' AND t_id='$t_id' ";
         $conn=dbConnect();
 
@@ -55,6 +61,32 @@
 
         return $freeTimes;
     }
+
+    function getByFreeTime($day, $timeSlot) {
+        $conn = dbConnect();
+
+        $day = mysqli_real_escape_string($conn, $day);
+        $timeSlot = mysqli_real_escape_string($conn, $timeSlot);
+
+        $query = "
+            SELECT *
+            FROM free_time
+            WHERE day='$day'
+            AND FIND_IN_SET('$timeSlot', free_times) > 0
+        ";
+
+        $data = mysqli_query($conn, $query);
+
+        $results = [];
+        if ($data && mysqli_num_rows($data) > 0) {
+            while ($row = mysqli_fetch_assoc($data)) {
+                $results[] = $row;
+            }
+        }
+
+        return $results;
+    }
+ 
     function getFreeTimeByDay($day)
     {
         $query = "SELECT * FROM free_time WHERE day='$day' ";
